37e79fd4146f74c3e32040ef1de7a95c7b50f947
rajakumar1099 <rajakumarsaravanan@gmail.com>
2020-11-13 01:35:07 +0530

    initial commit

--- app/src/main/java/com/androider/kotlin/ui/HomeFragment.kt
+++ app/src/main/java/com/androider/kotlin/ui/HomeFragment.kt
@@ -2,16 +2,27 @@ package com.androider.kotlin.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.androider.kotlin.R
+import com.androider.kotlin.ui.imageSlide.ImageSliderAdapter
 import com.androider.kotlin.utils.Constants
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlin.math.abs
 
 
 class HomeFragment : Fragment() {
 
+    private var imageURLList = mutableListOf<String>()
+    private var handler: Handler = Handler()
+    lateinit var runnable: Runnable
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,9 +36,14 @@ class HomeFragment : Fragment() {
             view.timeTableLay.visibility = View.GONE
         }
 
+        //Notice board banner
+        noticeBanner(view)
 
+
+
+        //Set User Profile Picture
         view.usernameTv.text = Constants.TempUsername
-        Glide.with(view.context).load(Constants.TempImageURL.toString()).into(view.profileImg)
+        Glide.with(view.context).load(Constants.TempImageURL).into(view.profileImg)
 
 
 
@@ -38,6 +54,47 @@ class HomeFragment : Fragment() {
         return view
     }
 
+    private fun noticeBanner(view: View){
+        addImageURLList()
+        view.imageSliderViewPager.adapter = ImageSliderAdapter(imageURLList)
+        view.imageSliderViewPager.clipToPadding = false
+        view.imageSliderViewPager.clipChildren = false
+        view.imageSliderViewPager.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer()
+        compositePageTransformer.addTransformer(MarginPageTransformer(10))
+        compositePageTransformer.addTransformer(ViewPager2.PageTransformer(){
+                page: View, position: Float ->
+            val float: Float = 1 - abs(position)
+            page.scaleY = 0.85f + float * 0.15f
+        })
+        view.imageSliderViewPager.setPageTransformer(compositePageTransformer)
+        view.imageSliderViewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable,10000)
+            }
+        })
+
+        runnable = Runnable{
+            view.imageSliderViewPager.currentItem = view.imageSliderViewPager.currentItem + 1
+        }
+        view.imageSliderViewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable,10000)
+    }
+
+
+
     private fun statusBarColor(){
         val window: Window? = activity?.window
         window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
@@ -47,6 +104,13 @@ class HomeFragment : Fragment() {
     private fun openAddUserPage(){
         startActivity(Intent(context,AddUserActivity::class.java))
     }
+
+    private fun addImageURLList(){
+        for(i in 1..5){
+            imageURLList.add("https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80")
+        }
+    }
+
 
 
 
--- app/src/main/java/com/androider/kotlin/ui/imageSlide/ImageSliderAdapter.kt
+++ app/src/main/java/com/androider/kotlin/ui/imageSlide/ImageSliderAdapter.kt
@@ -1,55 +1,46 @@
 package com.androider.kotlin.ui.imageSlide
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.androider.kotlin.R
 import com.bumptech.glide.Glide
-import com.smarteist.autoimageslider.SliderViewAdapter
-import java.util.*
 
 
-class ImageSliderAdapter(private val context: Context) :
-    SliderViewAdapter<ImageSliderAdapter.SliderAdapterVH>() {
-    private var mSliderItems: MutableList<SliderItem> = ArrayList()
-    fun renewItems(sliderItems: MutableList<SliderItem>) {
-        mSliderItems = sliderItems
-        notifyDataSetChanged()
+class ImageSliderAdapter(var imageURL: MutableList<String>) : RecyclerView.Adapter<ImageSliderAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_image_slider,parent,false))
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_image_slider, parent, false)
+        view.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        return ViewHolder(view)
     }
 
-    fun deleteItem(position: Int) {
-        mSliderItems.removeAt(position)
-        notifyDataSetChanged()
+    override fun getItemCount(): Int {
+        return imageURL.size
     }
 
-    fun addItem(sliderItem: SliderItem) {
-        mSliderItems.add(sliderItem)
-        notifyDataSetChanged()
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.itemView).load(imageURL[position]).into(holder.itemImageView)
+        holder.itemView.setOnClickListener(){
+            view : View ->
+            val position: Int = position
+            Toast.makeText(view.context,"$position",Toast.LENGTH_SHORT).show()
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup): SliderAdapterVH {
-        val inflater: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_image_slider, null)
-        return SliderAdapterVH(inflater)
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val itemImageView: ImageView = itemView.findViewById(R.id.iv_auto_image_slider)
     }
 
-    override fun onBindViewHolder(viewHolder: SliderAdapterVH, position: Int) {
 
-    }
-    override fun getCount(): Int {
-        return mSliderItems.size
-    }
-
-    inner class SliderAdapterVH(itemView: View?) :
-        ViewHolder(itemView) {
-        var itemView: View? = null
-        var imageViewBackground: ImageView? = null
-
-
-
-
-    }
 }
+
+
+
+
